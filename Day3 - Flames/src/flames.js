6f11c16a72cd2bd398a420a78d46b4fd2b6db6bd
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-07-02 20:56:19 +0530

    Update - flames .js

--- Day3 - Flames/src/flames.js
+++ Day3 - Flames/src/flames.js
@@ -7,9 +7,17 @@ class Flames extends React.Component{
         const a = "ranbir";
         const b = "alia";
         let c = a.split("").concat(b.split(""));
-            c = [... new Set(c)]
-        console.log(c)
+            c  = [ ...new Set(c)];
+        const flames = "flames";
+        const output = {
+            "f" : "friends",
+            "l" : "love",
+            "m" : "marriage",
+            "e" : "enemy",
+            "s" : "sisters"
+        }
         
+       
     }
 
 
