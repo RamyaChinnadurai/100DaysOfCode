6b4f902e9b8d3a9cb7376f711c9561ee6ea18a5e
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-08-28 23:57:44 +0530

    Add - dynamic routes

--- Day12 - nextjs-blog/lib/posts.js
+++ Day12 - nextjs-blog/lib/posts.js
@@ -28,3 +28,27 @@ export function getSortedPostsData() {
     }
   })
 }
+
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory)
+  
+    return fileNames.map(fileName => {
+      return {
+        params: {
+          id: fileName.replace(/\.md$/, '')
+        }
+      }
+    })
+  }
+
+  export function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+  
+    const matterResult = matter(fileContents)
+  
+    return {
+      id,
+      ...matterResult.data
+    }
+  }
---
+++ Day12 - nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,33 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+
+
+export async function getStaticPaths() {
+   const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const postData = getPostData(params.id)
+    return {
+        props: {
+        postData
+        }
+    } 
+}
+
+export default function Post({postData}){
+    return(
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+        </Layout>
+    )
+}
