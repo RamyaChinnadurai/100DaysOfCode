93133b852ea1f9513d253e18c6469bc0faca90b6
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-08-28 23:44:31 +0530

    Update - rams codes

--- Day12 - nextjs-blog/components/layout.js
+++ Day12 - nextjs-blog/components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Rams code'
+const name = 'Rams codes'
 export const siteTitle = 'Rams codes website'
 
 export default function Layout({ children, home }) {
