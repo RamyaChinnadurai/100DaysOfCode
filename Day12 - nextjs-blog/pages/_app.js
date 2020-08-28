c10ead4e74c2230cb6999c3544c15cba29a8b5ba
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-08-28 23:01:11 +0530

    Add - layout module css

---
+++ Day12 - nextjs-blog/pages/_app.js
@@ -0,0 +1,3 @@
+export default function App({ Components, pageProps}) {
+    return <Component {...pageProps} />
+}
--- Day12 - nextjs-blog/pages/posts/first-post.js
+++ Day12 - nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,24 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost(){
     return (
         <>
-            <h1> First post </h1>
-            <h2>
-                <Link href="/">
-                    <a> Back to home </a>
-                </Link>
-            </h2>
+            <Layout>
+                <Head>
+                    <title>
+                        First Post
+                    </title>
+                </Head>
+                <h1> First post </h1>
+                <h2>
+                    <Link href="/">
+                        <a> Back to home </a>
+                    </Link>
+                </h2>
+            </Layout>
+            
         </>
     )
 }
