78d515f6529696c05e70b723d245b4e899a9d18b
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-08-28 22:46:00 +0530

    Learned - link and prefetching in nextjs

--- Day12 - nextjs-blog/pages/posts/first-post.js
+++ Day12 - nextjs-blog/pages/posts/first-post.js
@@ -4,6 +4,11 @@ export default function FirstPost(){
     return (
         <>
             <h1> First post </h1>
+            <h2>
+                <Link href="/">
+                    <a> Back to home </a>
+                </Link>
+            </h2>
         </>
     )
 }
