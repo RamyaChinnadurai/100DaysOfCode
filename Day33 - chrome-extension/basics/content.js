8e6bdb3f2c45b7b225b66909180465d8465a25ff
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-09-18 19:23:34 +0530

    Update - events on button click

--- Day33 - chrome-extension/basics/content.js
+++ Day33 - chrome-extension/basics/content.js
@@ -1,15 +1,14 @@
 console.log("Chrome Extension go!");
 
-let paragraphs = document.getElementsByTagName('p');
-for( let i = 0; i< Object.keys(paragraphs).length; i++){
-    paragraphs[i].style['background-color'] = '#FF00FF';
-}
-
-console.log('its applied');
-
-
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse){
     console.log(message.text);
+    if(message.text === "hello"){
+        let paragraphs = document.getElementsByTagName('p');
+        for( let i = 0; i< Object.keys(paragraphs).length; i++){
+            paragraphs[i].style['background-color'] = '#FF00FF';
+        }
+        
+    }
 }
