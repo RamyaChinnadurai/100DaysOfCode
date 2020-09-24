dac758d5bc97fb83116691406a4e2c78d21b77c6
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-09-24 21:12:18 +0530

    Add - background js

---
+++ Day39 - wiki search/background.js
@@ -0,0 +1,27 @@
+const menuItem = {
+    "id": "wiki-search",
+    "title": "wiki search",
+    "contexts": ["selection"]
+}
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI(string){
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    const selectedText = clickData.selectedText;
+    if(clickData.menuItemId == "wiki-search" && selectedText) {
+        const wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(selectedText);
+        const request = {
+            "url" : wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };
+        chrome.windows.create(request, function(){});
+    }
+})
