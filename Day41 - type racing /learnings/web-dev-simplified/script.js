68e3c7e8344d52a4b82639f1e9840430751adc59
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-09-26 19:57:40 +0530

    Add - timer component

--- Day41 - type racing /learnings/web-dev-simplified/script.js
+++ Day41 - type racing /learnings/web-dev-simplified/script.js
@@ -1,6 +1,7 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTE_API_URL)
@@ -8,6 +9,28 @@ function getRandomQuote(){
     .then(data => data.content)
 }
 
+quoteInputElement.addEventListener('input', ()=> {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if(character == null){
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        }else if(character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        }else{
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            correct = false;
+        }
+    })
+
+    if(correct) renderNewQuote()
+})
 
 async function renderNewQuote(){
     const quote = await getRandomQuote();
@@ -18,6 +41,19 @@ async function renderNewQuote(){
         quoteDisplayElement.appendChild(characterSpan);
     });
     quoteInputElement.value = null;
+    startTimer();
 }
 
+let startTime;
+function startTimer(){
+    timerElement.innerText = 0;
+    startTime = new Date();
+    setInterval(()=>{
+        timer.innerText = getTimerTime();
+    },1000)
+}
+
+function getTimerTime(){
+    return Math.floor((new Date() - startTime) / 1000)
+}
 renderNewQuote();
