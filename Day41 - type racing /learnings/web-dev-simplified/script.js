e67b8bb336f2bd63e7ca79afbae8c04b8b52e675
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-09-26 18:55:32 +0530

    Update - new quote in the element

--- Day41 - type racing /learnings/web-dev-simplified/script.js
+++ Day41 - type racing /learnings/web-dev-simplified/script.js
@@ -1,4 +1,6 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTE_API_URL)
@@ -6,9 +8,10 @@ function getRandomQuote(){
     .then(data => data.content)
 }
 
-async function getNextQuote(){
+async function renderNewQuote(){
     const quote = await getRandomQuote();
-    console.log(quote);
+    quoteDisplayElement.innerText = quote;
+    quoteInputElement.value = null;
 }
 
-getNextQuote();
+renderNewQuote();
