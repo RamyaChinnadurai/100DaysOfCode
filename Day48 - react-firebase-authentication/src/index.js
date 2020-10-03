35bdacc0379c89fed9037e9a53e9e0c75fa7d8fc
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-10-03 17:44:23 +0530

    Add - connected firebase authentication

--- Day48 - react-firebase-authentication/src/index.js
+++ Day48 - react-firebase-authentication/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
