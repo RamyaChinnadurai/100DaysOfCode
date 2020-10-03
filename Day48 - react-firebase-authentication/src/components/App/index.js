d718bbc380db5422e245f0523ffd8ebb46db58db
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-10-03 12:41:06 +0530

    Add - base constant  values

--- Day48 - react-firebase-authentication/src/components/App/index.js
+++ Day48 - react-firebase-authentication/src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const App = () => {
-    <div>
-        <h1>App</h1>
-    </div>
+    return(
+        <div>
+             <h1>App</h1>
+        </div>
+    )
 }
 
 export default App;
