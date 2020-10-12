cbf81bbc422237aa4db7ad4811aebf92339c243c
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-10-12 22:56:40 +0530

    Update - snapshot for button

--- Day 57 - chrome firestore/react-jest/src/components/button/button.js
+++ Day 57 - chrome firestore/react-jest/src/components/button/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './button.css'
 
 function Button({label}){
-    return <div data-testid="button" className="button-style">{label}</div>
+    return <div data-testid="button" className="button-style">Hi {label}</div>
 }
 
 export default Button;
